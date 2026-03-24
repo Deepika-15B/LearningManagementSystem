9281f4a8c182592710515d9c8c3a8c70b940d065
Deepika-15B <baladeepika0510@gmail.com>
2026-03-24 20:21:06 +0530

    Changed vercel errors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://learning-management-system-seven-ruddy.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
